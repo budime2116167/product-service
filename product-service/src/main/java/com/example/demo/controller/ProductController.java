84da7f76911e46c87531c114d21f75b9ae3032a8
venkat <budime1205@gmail.com>
2021-12-07 23:36:20 +0000

    [DDEP-2103] first commit

--- product-service/src/main/java/com/example/demo/controller/ProductController.java
+++ product-service/src/main/java/com/example/demo/controller/ProductController.java
@@ -5,6 +5,8 @@ public class ProductController {
 	
 	public String hello() {
 		System.out.println("hello spring boot");
+		System.out.println("hello manju git");
+		
 		
 		return "hello spring boot1";
 	}
---
+++ product-service/src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+public class ProductService {
+	
+	public void message() {
+		System.out.println("Inside Product Service");
+	}
+
+}
