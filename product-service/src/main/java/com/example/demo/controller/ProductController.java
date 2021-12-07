6c2548e7d8d308892ac3e600be487fc320729a56
venkat <budime1205@gmail.com>
2021-12-07 19:35:47 +0000

    commit 4th

---
+++ product-service/src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+
+public class ProductController {
+	
+	public String hello() {
+		System.out.println("hello spring boot");
+		
+		return "hello spring boot1";
+	}
+
+}
